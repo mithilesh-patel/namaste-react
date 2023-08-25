637d8dd7f22b8053b2e3d7366331ec8c35990e48
mithileshpatel <mithileshpatel250@gmail.com>
2023-08-25 22:48:54 +0530

    Igniting the App using Parcel

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
 const heading= React.createElement("h1", {id:'heading1'}, "Heading 1");
 const heading2= React.createElement("h2", {}, "Heading 2");
 
