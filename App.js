ebc3db94b625207cbaeb26cc31ef1784ffc1ef3d
mithileshpatel <mithileshpatel250@gmail.com>
2023-08-29 23:51:48 +0530

    .babelrc configuration file added

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+React.createElement("h2", {}, "Heading 2")
 
 
-const heading= React.createElement("h1", {id:'heading1'}, "Heading 1 from parcel");
-const heading2= React.createElement("h2", {}, "Heading 2");
+const container=React.createElement('div',{id:'container'},
+ [ React.createElement("h1", {id:'heading1'}, "Heading 1 from parcel"),
+ React.createElement("h2", {}, [
+    React.createElement("h4", {id:'heading4'}, "This is heading4"),
+    React.createElement("h4", {id:'heading4'}, "This is heading4")
 
-const container=React.createElement('div',{id:'container'}, [heading, heading2])
+    ])
+ ])
 
 
 const root= ReactDOM.createRoot(document.getElementById('root'));
